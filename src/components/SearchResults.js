95f71c3824a66748fa8a8d8aea3396a48272ea2e
Gingaling <nycgingsf@hotmail.com>
2022-07-31 14:29:07 -0700

    Committng files

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const SearchResults = ({ images }) => {
 	// return early if there are no images
-	if (!images.length) {
+	if ( {images.length} === 0 ) {
 		return <h2>No Images Found!</h2>;
 	}
-
 	return <div className="gallery">
 			{images.map(image => <div key={image.id} className="gif">
 					<img alt="" src={image.images.downsized_large.url} />
