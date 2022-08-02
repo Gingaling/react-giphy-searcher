f9a1a4eabd1b04dff18f73aa0ddfe102eb48a94f
Gingaling <nycgingsf@hotmail.com>
2022-08-01 20:21:38 -0700

    Committing files with changes to code to correct for deployment issues

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 
 const SearchResults = ({images}) => {
+		console.log(images);
+
 	// return early if there are no images
 	if (images.length === 0) {
 		console.log(typeof images);
+		console.log(images);
 		console.log(typeof images.length);
 		return <h2>No Images Found!</h2>;
 	}
