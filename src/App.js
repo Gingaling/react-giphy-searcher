9a7390b050abd6c387d05c0ae8125ed479aa9c0f
Gingaling <nycgingsf@hotmail.com>
2022-08-02 09:59:53 -0700

    Committing files with changes to code to correct for deployment issues

--- src/App.js
+++ src/App.js
@@ -43,6 +43,23 @@ function App() {
 		getImages();
 	}
 
+const SearchResults = ({images}) => {
+		console.log(images);
+
+	// return early if there are no images
+	if (images.length === 0) {
+		console.log(typeof images);
+		console.log(images);
+		console.log(typeof images.length);
+		return <h2>No Images Found!</h2>;
+	}
+	return <div className="gallery" id="galleria">
+			{images.map(image => <div key={image.id} className="gif">
+					<img alt="" src={image.images.downsized_large.url} />
+				</div>)}
+		</div>;
+};
+
 	return <div>
 			<img id="logo" src={logo} alt="" />
 			<h1 id="titleHead">Giphy Searcher</h1>
